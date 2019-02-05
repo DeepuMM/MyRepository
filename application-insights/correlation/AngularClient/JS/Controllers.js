6428ac6e64eb6231f2a53c793b09c4157129859e
deeputtan <deepummp@gmail.com>
2019-02-05 12:14:59 +0530

    Add files via upload

---
+++ application-insights/correlation/AngularClient/JS/Controllers.js
@@ -0,0 +1,47 @@
+﻿/// <reference path="../angular.min.js" />
+/// <reference path="Modules.js" />
+/// <reference path="Services.js" />
+
+app.controller("RESTClientController", ['$scope', '$log', 'RESTClientService', function ($scope, $log, RESTClientService) {
+    function createGuid() {
+        return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
+    $scope.testHeaders = function () {
+        //alert("test headers");
+        RESTClientService.EchoHeaders()
+        .then(function (data) {
+            alert(JSON.stringify( data, null, ' '));
+        });
+    }
+    $scope.findArea = function () {
+        $scope.operation_id = createGuid();
+        window.appInsights.context.operation.id = $scope.operation_id;
+        window.appInsights.trackEvent("Custom event from JS before service call", { operation_Id: $scope.operation_id });
+
+        var promiseGet = RESTClientService.get($scope.radius, $scope.operation_id);
+        promiseGet.then(function (pl) {
+            $scope.message = pl.data;
+        },
+                  function (errorPl) {
+                      $log.error('failure loading Company', errorPl);
+                  });
+    }
+
+    $scope.firstAI = function () {
+        $scope.operation_id = createGuid();
+        window.appInsights.queue.push(function () {
+            window.appInsights.context.addTelemetryInitializer(function (envelope) {
+                envelope.tags["ai.operation.id"] = $scope.operation_id;
+                envelope.tags["ai.operation.name"] = "my-client-custom-init-op";
+                console.log("'ai.operation.id' init " + $scope.operation_id);
+            });
+        });
+        window.appInsights.trackEvent("Custom init event from JS", { operation_Id: $scope.operation_id });
+
+    }
+    $scope.firstAI();
+}]);
+
---
+++ application-insights/correlation/FrontEndWCFService/FrontEndService.svc.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrontEndWCFService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class FrontEndService : IFrontEndService
+    {
+        public string GetAreaOfCircle(string value)
+        {
+            TelemetryClient client = GetTelemetryClient();
+            IOperationHolder<DependencyTelemetry> holder = client.StartOperation<DependencyTelemetry>("Custom operation from FrontEndWCFService");
+            holder.Telemetry.Type = "Custom";
+            double radius = Convert.ToDouble(value);
+            double pi = GetValueOfPi().Result;
+            client.StopOperation<DependencyTelemetry>(holder);
+            return string.Format("Area of circle with {0} raidus is {1}", value, pi * radius * radius);
+        }
+
+        private static TelemetryClient GetTelemetryClient()
+        {
+            return new TelemetryClient(TelemetryConfiguration.Active);
+        }
+        static readonly Random random = new Random();
+        private static async Task<double> GetValueOfPi()
+        {
+            InternalServiceReference.InternalServiceClient serviceClient = new InternalServiceReference.InternalServiceClient();
+            double pi;
+            bool shouldGetPiFromService = random.Next() % 2 == 0;
+            shouldGetPiFromService = false; // Hack if the netPipeService is not working.
+            if (shouldGetPiFromService) {
+                pi = await serviceClient.GetValueOfPiAsync();
+                //This public URL will also give the valyue. https://api.pi.delivery/v1/pi?start=0&numberOfDigits=10
+            }
+            else
+            {
+                pi = 3.14;
+            }
+            
+            await Task.Delay(1000);
+            GetTelemetryClient().TrackEvent($"FrontEndService.GetValueOfPi() = {pi}");
+            
+            return pi;
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
